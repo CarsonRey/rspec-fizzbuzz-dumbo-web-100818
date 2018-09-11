c02370eddb43876c2424ac0c7708629a099fb595
Carson Rey <c.rey494@gmail.com>
2018-09-11 18:07:05 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,7 @@ require 'pry'
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 5 == 0 && int % 3 == 0
-    "Fizzbuzz"
+    "FizzBuzz"
   elsif int % 5 == 0 
     "Buzz"
   elsif int % 3 == 0 
